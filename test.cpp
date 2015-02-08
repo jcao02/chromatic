572a60e2d3d46510ed4d41f6ec881110997c8a1e
Juan Carlos Arocha <arochajuan2@gmail.com>
2015-02-08 10:22:54 -0430

    Adding CMake Adding gtest library Adding test.cpp file

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+
+TEST(Clique, BuildClique) {
+    EXPECT_EQ(0, 1); 
+}
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS(); 
+}
